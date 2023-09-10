8170b2583a99809df0d6cbc1c852637f9036998e
Abdul Sobur Ademoye <Gbeminiyiask@gmail.com>
2023-09-10 14:11:15 +0100

    day1 updated

---
+++ day1 8kyu/app.js
@@ -0,0 +1,12 @@
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str) {
+  //   split strings
+  let string = str.split("").reverse().join("");
+  return string;
+
+  //   reverse the array
+  //   join the array
+}
+
+// 'world' becomes 'dlrow'
